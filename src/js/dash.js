996d0ffeada0546c7b92d4869172a26060995fa0
Harry Fender <kentuckyfriedmeerkat@gmail.com>
2017-05-09 22:04:02 +0100

    Move dashboard router into its own file

--- src/js/dash.js
+++ src/js/dash.js
@@ -1,17 +1,3 @@
-import $ from 'cash-dom';
-import Navigo from 'navigo';
-import Axios from 'axios';
+import * as DashRouter from './dash/router';
 
-let elem = $('#content');
-let defaultRoute = 'bug';
-
-let fillElem = template =>
-    Axios.get(`/dash/templates/${template}`)
-        .then(response => elem.html(response.data))
-        .catch(error => router.navigate(`/page/${defaultRoute}`));
-
-let router = new Navigo();
-router
-    .on('/page/:id', params => fillElem(params.id))
-    .on('*', () => router.navigate(`/page/${defaultRoute}`))
-    .resolve();
+DashRouter.Init();
