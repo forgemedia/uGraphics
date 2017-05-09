e00aa73c3594d5c0f3dd3bc629d6449baf1a09d8
Harry Fender <kentuckyfriedmeerkat@gmail.com>
2017-05-09 21:19:53 +0100

    Fix dashboard routing

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,15 @@ import Navigo from 'navigo';
 import Axios from 'axios';
 
 let elem = $('#content');
+let defaultRoute = 'bug';
 
 let fillElem = template =>
     Axios.get(`/dash/templates/${template}`)
         .then(response => elem.html(response.data))
-        .catch(error => console.dir(error));
+        .catch(error => router.navigate(`/page/${defaultRoute}`));
 
 let router = new Navigo();
 router
     .on('/page/:id', params => fillElem(params.id))
-    .on('*', () => router.navigate('/page/default'))
+    .on('*', () => router.navigate(`/page/${defaultRoute}`))
     .resolve();
