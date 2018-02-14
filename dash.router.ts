b7519faa056ae5126cb0c8562ec01d9190d9c0fc
Harry Fender <kentuckyfriedmeerkat@gmail.com>
2018-02-14 20:34:55 +0000

    Ability to change bug image

--- dash.router.ts
+++ dash.router.ts
@@ -1,12 +1,19 @@
 // Import the Express module
 import * as Express from 'express';
+import * as Shell from 'shelljs';
+import * as _ from 'lodash';
+import * as Path from 'path';
 
 // Create an Express router
 let router = Express.Router();
 
 // Serve Pug dashboard templates
 router.get('/templates/:id', (req, res) =>
-    res.render(`dash/templates/${req.params.id}`));
+    res.render(`dash/templates/${req.params.id}`, {
+        Shell: Shell,
+        _: _,
+        Path: Path
+    }));
 
 // For all other requests to this router, just render the index template
 router.get('/*', (req, res) => res.render('dash/index'));
