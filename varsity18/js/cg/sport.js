7fe0ca9060fe176c2fd517ea540ffba649479d03
Harry Fender <kentuckyfriedmeerkat@gmail.com>
2018-03-23 20:37:00 +0000

    Remove old timer and move subtimer to new platform

--- varsity18/js/cg/sport.js
+++ varsity18/js/cg/sport.js
@@ -3,12 +3,10 @@ import Rivets from 'rivets';
 import _ from 'lodash';
 
 import cgController from 'js/cg/cgController';
-import Timer from 'js/cg/modules/timer';
 
 /** @ignore */
 export default class extends cgController {
     constructor() {
         super('sport');
-        Timer(this);
     }
 }
