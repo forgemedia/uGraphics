a962c97ba5c648182993f36385ad9623f1420c01
Harry Fender <kentuckyfriedmeerkat@gmail.com>
2018-03-25 21:32:32 +0100

    Mechanism dynamic module loading

--- mechanismRegister.js
+++ mechanismRegister.js
@@ -1,10 +1,13 @@
-import Timer from './mechanism/timer';
+import { Config } from './shared';
 
-let mechanisms = {
-    timer: Timer
-};
+let mechanisms = {};
+// dynamic module loading lol
+for (let mechanism in Config.mechanisms) {
+    mechanisms[mechanism] = require(`./${Config.mechanisms[mechanism]}`);
+}
 
 export let Handle = (msg, dataStore, callback) => {
     if (!mechanisms[msg.id]) return false;
     mechanisms[msg.id](msg.data, dataStore, callback);
+    return true;
 };
